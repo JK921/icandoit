c210507cdf2d1d947a648fdb1e313573d4d85b84
JK <kmanddang@gmail.net>
2018-06-26 00:37:14 +0900

    숨바꼭질 문제풀이 추가

---
+++ queue/README.md
@@ -0,0 +1,25 @@
+# 큐 사용하기
+> BFS를 이용
+
+* 상태의 갯수 **1초 내로 계산** 가능
+* **최소**를 구하는 문제 (최단경로)
+* 간선의 **가중치가 1**
+
+
+### 문제 풀이
+#### 숨바꼭질
+* src/Solution1697.java
+* input/1697.txt
+* [https://www.acmicpc.net/problem/1697](https://www.acmicpc.net/problem/1697)
+
+##### 회고
+* queue 사용법 숙지
+```java
+Queue<Integer> queue = new LinkedList<Integer>();
+```
+* 범위 주의
+ **N(0 ≤ N ≤ 100,000)** 
+
+
+
+
---
+++ queue/src/queue/Solution1697.java
@@ -0,0 +1,57 @@
+package queue;
+
+import java.io.FileInputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Solution1697 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new FileInputStream("./input/1697.txt"));
+
+        int cur = sc.nextInt();
+        int dest = sc.nextInt();
+
+        boolean[] visited = new boolean[200000];
+        int[] dist = new int[200000];
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        dist[cur] = 0;
+        queue.add(cur);
+        visited[cur] = true;
+
+        while (!queue.isEmpty()) {
+            cur = queue.remove();
+
+            if (cur == dest) {
+                break;
+            }
+
+            // -1
+            if (0 <= cur - 1 && !visited[cur - 1]) {
+                queue.add(cur - 1);
+                dist[cur - 1] = dist[cur] + 1;
+                visited[cur - 1] = true;
+            }
+
+            // +1
+            if (cur + 1 < 200000 && !visited[cur + 1]) {
+                queue.add(cur + 1);
+                dist[cur + 1] = dist[cur] + 1;
+                visited[cur + 1] = true;
+            }
+
+            // x2
+            if (cur * 2 < 200000 && !visited[cur * 2]) {
+                queue.add(cur * 2);
+                dist[cur * 2] = dist[cur] + 1;
+                visited[cur * 2] = true;
+            }
+
+        }
+        System.out.println(dist[dest]);
+
+        sc.close();
+    }
+}
